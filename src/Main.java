77f23c6207af6722d84721fffb94645b6b26b91c
InactiveGaming13 <116043268+InactiveGaming13@users.noreply.github.com>
2025-02-06 22:50:05 +1000

    Attempted object parsing between the client and the server

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
+import java.lang.reflect.Array;
 import java.net.*;
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
+    private ObjectInputStream objIn;
 
-    public void start(int port) throws IOException {
+    public void start(int port) throws IOException, ClassNotFoundException {
         serverSocket = new ServerSocket(port);
         InetAddress serverAddress = serverSocket.getInetAddress();
         System.out.println("Server started at " + serverAddress.getHostAddress() + ":" + port);
         clientSocket = serverSocket.accept();
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        objIn = new ObjectInputStream(clientSocket.getInputStream());
 
         String inputLine;
         while ((inputLine = in.readLine()) != null) {
@@ -24,13 +28,12 @@ public class Main {
             out.println(inputLine);
         }
 
-        String greeting = in.readLine();
-        if ("hello server".equals(greeting)) {
-            out.println("hello client");
-        }
-        else {
-            out.println("unrecognised greeting");
-        }
+        Array[] arr = (Array[]) objIn.readObject();
+        System.out.println("Received Object: " + Arrays.toString(arr));
+
+        String message = in.readLine();
+        System.out.println("Received Message: " + message);
+
     }
 
     public void stop() throws IOException {
@@ -40,7 +43,7 @@ public class Main {
         serverSocket.close();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Main server=new Main();
         server.start(6666);
     }
